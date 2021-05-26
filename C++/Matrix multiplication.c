2668ec83025e41b6d75e4ef9d242033221d55a00
sayande717 <sayande4203@protonmail.com>
2021-05-26 23:56:03 +0530

    .

--- C++/Matrix multiplication.c
+++ C++/Matrix multiplication.c
@@ -1,24 +1,27 @@
 /* Matrix multiplication */
 #include<stdio.h>
+#include<limits.h>
 int main()
 {
-  int m, n, p, q, c, d, k;
-  int a[10][10], b[10][10], mul[10][10];
+  Variable description - 
+  r1,c1 = number of rows and columns of the first matrix.
+  r1,c2 = number of rows and columns of the second matrix.
+  
+  int n1,n2;
+  int a1[INT_MAX][INT_MAX], a2[INT_MAX][INT_MAX], a3[INT_MAX][INT_MAX];
   printf("Enter number of rows and columns of first matrix\n");
-  scanf("%d%d", &m, &n);
-  printf("Enter elements of first matrix\n");
- 
-  for (c = 0; c < m; c++)
-    for (d = 0; d < n; d++)
-      scanf("%d", &a[c][d]);
- 
+  scanf("%d%d", &r1,&c1);
   printf("Enter number of rows and columns of second matrix\n");
   scanf("%d%d", &p, &q);
- 
   if (n != p)
     printf("The multiplication isn't possible.\n");
   else
   {
+    printf("Enter elements:\nMatrix 1");
+    printf("Enter elements of first matrix\n");
+  for (r1 = 0; r1 < m; r1++)
+    for (d = 0; d < n; d++)
+      scanf("%d", &a[c][d]);
     printf("Enter elements of second matrix\n");
  
     for (c = 0; c < p; c++)
