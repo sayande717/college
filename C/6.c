1b9eae30bb252eedb8a5076ec46522f6aafb86f2
sayande717 <sayande4203@protonmail.com>
2021-09-15 11:48:39 +0530

    .

--- C/6.c
+++ C/6.c
@@ -1,16 +1,16 @@
 //Program to print the fibonacci series.
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-int n,i,ti=0,tf=1,s;
-printf("Enter the number of terms:");
-scanf("%d",&n);
-for(i=1;i<=n;i++)
-{
-printf("%d ",ti);
-printf("\n");
-s=ti+tf;
-ti=tf;
-tf=s;
-}
+    int n, i, ti = 0, tf = 1, s;
+    printf("Enter the number of terms:");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d ", ti);
+        printf("\n");
+        s = ti + tf;
+        ti = tf;
+        tf = s;
+    }
 }
