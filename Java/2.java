f9515e4443182b010a3081715babe3b29bb78a2b
Sayan De <sayande4203@protonmail.com>
2021-11-02 09:59:53 +0530

    .

--- Java/2.java
+++ Java/2.java
@@ -1,5 +1,6 @@
 class circle {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         double radius = 5, perimeter,area = 3.14 * radius * radius;
         perimeter = 2 * 3.14 * radius;
         System.out.println(area);
